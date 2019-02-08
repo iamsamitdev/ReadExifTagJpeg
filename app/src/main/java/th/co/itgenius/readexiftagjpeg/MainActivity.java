92bdbd4ebc07fcc9ce5cf40533b6bc87a46dd790
Samit Koyom <samitkoyom@gmail.com>
2019-02-08 16:57:39 +0700

    initail commit

---
+++ app/src/main/java/th/co/itgenius/readexiftagjpeg/MainActivity.java
@@ -0,0 +1,275 @@
+package th.co.itgenius.readexiftagjpeg;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
+
+    private static final int RQS_OPEN_IMAGE = 1;
+
+    Button buttonOpen;
+    TextView textUri;
+    ImageView imageView;
+
+    Uri targetUri = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonOpen = findViewById(R.id.opendocument);
+        textUri = findViewById(R.id.texturi);
+        imageView = findViewById(R.id.image);
+
+        buttonOpen.setOnClickListener(buttonOpenOnClickListener);
+        textUri.setOnClickListener(textUriOnClickListener);
+        imageView.setOnClickListener(imageOnClickListener);
+    }
+
+    // Android Permission ========================================================
+    public boolean checkPermissionREAD_EXTERNAL_STORAGE(
+            final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context, Manifest.permission.READ_EXTERNAL_STORAGE);
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) context,
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    public void showDialog(final String msg, final Context context, final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[]{permission},
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // do your stuff
+                } else {
+                    Toast.makeText(getApplicationContext(), "GET_ACCOUNTS Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions,
+                        grantResults);
+        }
+    }
+
+    /// ======================================================================================
+
+    View.OnClickListener buttonOpenOnClickListener =
+            new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if (checkPermissionREAD_EXTERNAL_STORAGE(MainActivity.this)) {
+                        Intent intent = new Intent();
+                        intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
+                        intent.addCategory(Intent.CATEGORY_OPENABLE);
+                        intent.setType("image/jpeg");
+                        startActivityForResult(intent, RQS_OPEN_IMAGE);
+                        // do your stuff..
+                    }
+                }
+            };
+
+    View.OnClickListener textUriOnClickListener =
+            new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if (targetUri != null) {
+                        Bitmap bm;
+                        try {
+                            bm = BitmapFactory.decodeStream(
+                                    getContentResolver()
+                                            .openInputStream(targetUri));
+                            imageView.setImageBitmap(bm);
+                        } catch (FileNotFoundException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            };
+
+    View.OnClickListener imageOnClickListener =
+            new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    showExif(targetUri);
+                }
+            };
+
+    void showExif(Uri photoUri) {
+        if (photoUri != null) {
+
+            String photoPath = getRealPathFromURI(photoUri);
+
+            try {
+                /*
+                ExifInterface (String filename) added in API level 5
+                 */
+                ExifInterface exifInterface = new ExifInterface(photoPath);
+
+                String exif = "Exif: ";
+                exif += "\nIMAGE_LENGTH: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_IMAGE_LENGTH);
+                exif += "\nIMAGE_WIDTH: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_IMAGE_WIDTH);
+                exif += "\n DATETIME: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_DATETIME);
+                exif += "\n TAG_MAKE: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_MAKE);
+                exif += "\n TAG_MODEL: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_MODEL);
+                exif += "\n TAG_ORIENTATION: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_ORIENTATION);
+                exif += "\n TAG_WHITE_BALANCE: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_WHITE_BALANCE);
+                exif += "\n TAG_FOCAL_LENGTH: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_FOCAL_LENGTH);
+                exif += "\n TAG_FLASH: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_FLASH);
+                exif += "\nGPS related:";
+                exif += "\n TAG_GPS_DATESTAMP: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_DATESTAMP);
+                exif += "\n TAG_GPS_TIMESTAMP: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_TIMESTAMP);
+                exif += "\n TAG_GPS_LATITUDE: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
+                exif += "\n TAG_GPS_LATITUDE_REF: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF);
+                exif += "\n TAG_GPS_LONGITUDE: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
+                exif += "\n TAG_GPS_LONGITUDE_REF: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF);
+                exif += "\n TAG_GPS_PROCESSING_METHOD: " +
+                        exifInterface.getAttribute(ExifInterface.TAG_GPS_PROCESSING_METHOD);
+
+                Toast.makeText(getApplicationContext(),
+                        exif,
+                        Toast.LENGTH_LONG).show();
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),
+                        "Something wrong:\n" + e.toString(),
+                        Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),
+                        "Something wrong:\n" + e.toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+
+        } else {
+            Toast.makeText(getApplicationContext(),
+                    "photoUri == null",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    private String getRealPathFromURI(Uri uri) {
+        String filePath = "";
+        String wholeID = DocumentsContract.getDocumentId(uri);
+
+        // Split at colon, use second item in the array
+        String id = wholeID.split(":")[1];
+
+        String[] column = {MediaStore.Images.Media.DATA};
+
+        // where id is equal to
+        String sel = MediaStore.Images.Media._ID + "=?";
+
+        Cursor cursor = getApplicationContext().getContentResolver()
+                .query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        column, sel, new String[]{id}, null);
+
+        int columnIndex = cursor.getColumnIndex(column[0]);
+
+        if (cursor.moveToFirst()) {
+            filePath = cursor.getString(columnIndex);
+        }
+        cursor.close();
+        return filePath;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+
+            Uri dataUri = data.getData();
+
+            if (requestCode == RQS_OPEN_IMAGE) {
+                targetUri = dataUri;
+                textUri.setText(dataUri.toString());
+                imageView.setImageBitmap(null);
+            }
+        }
+    }
+}
